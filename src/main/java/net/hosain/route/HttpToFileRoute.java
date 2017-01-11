9bdd786d0590224e08d2e75e9f33fd162890a87b
Hosain <contact@hosain.net>
2017-01-11 17:20:23 +0000

    Added sample camel route with an integration test

---
+++ src/main/java/net/hosain/route/HttpToFileRoute.java
@@ -0,0 +1,16 @@
+package net.hosain.route;
+
+import org.apache.camel.builder.RouteBuilder;
+import org.springframework.stereotype.Component;
+
+@Component
+public class HttpToFileRoute extends RouteBuilder {
+
+    @Override
+    public void configure() throws Exception {
+        from("timer://foo?period=5000&delay=5s")
+                        .id("remoteResourceRoute")
+                        .to("{{remote.host}}")
+                        .to("file:{{output.dir.path}}?fileName=output.txt");
+    }
+}
---
+++ src/test/java/net/hosain/route/HttpToFileRouteIT.java
@@ -0,0 +1,52 @@
+package net.hosain.route;
+
+import org.apache.camel.CamelContext;
+import org.apache.camel.api.management.mbean.ManagedRouteMBean;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import java.io.File;
+
+import static com.jayway.awaitility.Awaitility.await;
+import static java.util.concurrent.TimeUnit.SECONDS;
+import static org.assertj.core.api.Assertions.assertThat;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+public class HttpToFileRouteIT {
+
+    @Autowired
+    private CamelContext camelContext;
+
+    private File outFile;
+
+    @After
+    public void cleanup() {
+        try {
+            outFile.delete();
+
+        } catch (Exception ignored) {
+        }
+    }
+
+    @Test
+    public void testRouteRequestsResourcesAndWritesResponseToFile() throws Exception {
+        //given
+        outFile = new File("target/output.txt");
+
+        //when
+        await().atMost(10, SECONDS).until(() -> getTotalExchanges() == 1);
+
+        //then
+        assertThat(outFile.exists()).isTrue();
+    }
+
+    private long getTotalExchanges() throws Exception {
+        return camelContext.getManagedRoute("remoteResourceRoute", ManagedRouteMBean.class).getExchangesTotal();
+    }
+}

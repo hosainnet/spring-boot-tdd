96be79cd9742326c28a7a9ed9b028065ac0d2c36
Hosain <contact@hosain.net>
2017-01-12 13:17:07 +0000

    Added @Component annotation to QuoteDao

--- src/main/java/net/hosain/dao/QuoteDao.java
+++ src/main/java/net/hosain/dao/QuoteDao.java
@@ -1,8 +1,11 @@
 package net.hosain.dao;
 
+import org.springframework.stereotype.Component;
+
 import java.time.DayOfWeek;
 import java.util.List;
 
+@Component
 public class QuoteDao {
     public List<String> findByDayAndMarketing(DayOfWeek day, boolean marketing) {
         return null;
